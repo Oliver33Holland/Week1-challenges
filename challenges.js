97b6cc5bcc3ea64225b566362dd4b4549eec6ea3
Oliver33Holland <oliverholland33@gmail.com>
2023-05-03 10:44:30 +0100

    Week1

--- challenges.js
+++ challenges.js
@@ -6,5 +6,54 @@ console.log(' Removed Element:', removedEl);
 
 console.log('Remaining array', favDrinks); 
 
-console.log(Math.round(Math.random()*120))
+removedEl = favDrinks.shift();
+
+console.log(' Removed Element:', removedEl);
+
+console.log('Remaining array', favDrinks); 
+
+console.log(Math.floor(Math.random()*79));
+
+{console.log(Math.floor(Math.random()*6));
+let num = "2"
+if (num === "1"){
+console.log("you rolled a one")
+}else if (num === "2"){
+console.log("you rolled a two")
+}else if (num === "3"){
+console.log("you rolled a three")
+}else if (num === "4"){ 
+console.log("you rolled a four")
+}else if (num === "5"){ 
+console.log("you rolled a five")
+}else if (num === "6"){ 
+console.log("you rolled a six")
+}
+}
+
+let password = 'Password123'
+    if(password = ('<6')){
+    console.log ("password is too short")
+    function checkFunction(password)
+    }else if(password ('>6')){
+  console.log ("thank you")
+    }
+
+let bool1 = Boolean(10>9)
+if(Boolean(10>9)){
+    console.log("true")
+}else if(Boolean(10<9)){
+    console.log("false")
+}
+
+let num = 15
+if(num = 15/3){
+    console.log("fizz")
+}else if(num = 15/5){
+    console.log("buzz")
+}else if(num = 15/3){
+    console.log("fizz buzz")
+}
+
+
 
